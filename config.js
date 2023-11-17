3c40ff4fa3f7c391f5c7c6e799ac9987a8936faf
Nothingelser <129436420+Nothingelser@users.noreply.github.com>
2023-11-17 08:36:18 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['19177208293', 'JAYSON WHATSBOT', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
